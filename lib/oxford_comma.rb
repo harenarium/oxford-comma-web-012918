4f5d3e61fa8245e9dae180c3e9dfc34c0deb6703
Caroline Lee <carolinel2610@gmail.com>
2018-01-12 01:36:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.count
+  when 0
+    ""
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    last = array.pop
+    array.join(", ") + ", and " + last
+  end
 end
